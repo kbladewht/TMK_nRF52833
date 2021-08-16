75d7980222699cd8b3099b9b1eac3f639a574a40
Pismis24 <pismis24_1@163.com>
2021-08-16 17:30:40 +0800

    Revert "Revert "Config VDD voltage and Fix P0.06""
    This reverts commit 9b72168e9663067127d542b014ad90507c4711ce.

---
+++ SRC/BSP/board_support.c
@@ -0,0 +1,28 @@
+#include "board_support.h"
+
+
+// setting VDD and GPIO voltage as 3.3V when power via VDDH
+static void gpio_output_voltage_setup(void)
+{
+    // Configure UICR_REGOUT0 register only if it is set to default value.
+    if ((NRF_UICR->REGOUT0 & UICR_REGOUT0_VOUT_Msk) ==
+        (UICR_REGOUT0_VOUT_DEFAULT << UICR_REGOUT0_VOUT_Pos))
+    {
+        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen;
+        while (NRF_NVMC->READY == NVMC_READY_READY_Busy){}
+
+        NRF_UICR->REGOUT0 = (NRF_UICR->REGOUT0 & ~((uint32_t)UICR_REGOUT0_VOUT_Msk)) |
+                            (UICR_REGOUT0_VOUT_3V3 << UICR_REGOUT0_VOUT_Pos);
+
+        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren;
+        while (NRF_NVMC->READY == NVMC_READY_READY_Busy){}
+
+        // System reset is needed to update UICR registers.
+        NVIC_SystemReset();
+    }
+}
+
+void board_init()
+{
+    gpio_output_voltage_setup();
+}
